e95c9fdda718010071245869fc7d6f00c3707631
theus.hossmann@gmail.com <theus.hossmann@gmail.com@124ae47a-95dc-19e3-5197-fa75647cb349>
2011-10-25 12:23:43 +0000

    fixed user notification bug(s) and made notifications configurable in settings.

--- src/ch/ethz/twimight/Constants.java
+++ src/ch/ethz/twimight/Constants.java
@@ -5,10 +5,15 @@ final class Constants {
     
 	private Constants() { throw new AssertionError("Constants is uninstantiable"); }
     
+	// Special Hashtags
     public static final String TWHISPER_HASHTAG = "#twhisper";
     public static final String TWINTERNAL_HASHTAG = "#twinternal";
     
     // Intent Actions
     public static final String ACTION_NEW_DISASTER_TWEET = "New disaster tweet";
     public static final String ACTION_DISASTER_MODE = "Disaster Mode";
+    
+    // Update intervals
+    public static final long TWEET_UPDATE_INTERVAL = 110000L; // this is also for mentions and DMs!
+    public static final long FRIENDS_UPDATE_INTERVAL = 3600000L; //290000L; // for followers and followees 
 }
