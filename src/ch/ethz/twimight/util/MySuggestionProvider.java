b346a38a7fa3c589d37c537383b7aed8dfb52d9a
theus.hossmann@gmail.com <theus.hossmann@gmail.com@124ae47a-95dc-19e3-5197-fa75647cb349>
2011-11-06 09:04:34 +0000

    renamed activities (more meaningful names)

--- src/ch/ethz/twimight/util/MySuggestionProvider.java
+++ src/ch/ethz/twimight/util/MySuggestionProvider.java
@@ -1,10 +1,10 @@
-package ch.ethz.twimight;
+package ch.ethz.twimight.util;
 
 import android.content.SearchRecentSuggestionsProvider;
 
 
 public class MySuggestionProvider extends SearchRecentSuggestionsProvider {
-    public final static String AUTHORITY = "ch.ethz.twitteth";
+    public final static String AUTHORITY = "ch.ethz.twimight";
     public final static int MODE = DATABASE_MODE_QUERIES;
 
     public MySuggestionProvider() {
