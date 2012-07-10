2436debfd5c80944fe67be8de8f8d548a89f8cab
ing.pcarta@gmail.com <ing.pcarta@gmail.com@124ae47a-95dc-19e3-5197-fa75647cb349>
2012-07-10 17:45:47 +0000

    working on feedback

---
+++ branches/twimight_ng/src/ch/ethz/twimight/activities/FeedbackActivity.java
@@ -0,0 +1,18 @@
+package ch.ethz.twimight.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FeedbackActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	
+	
+
+}
