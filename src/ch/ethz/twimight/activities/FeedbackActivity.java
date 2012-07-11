4c33b6ea7043c13f846f9a3fdec4c4e6ab575c11
ing.pcarta@gmail.com <ing.pcarta@gmail.com@124ae47a-95dc-19e3-5197-fa75647cb349>
2012-07-11 13:50:47 +0000

    working on feedback

--- src/ch/ethz/twimight/activities/FeedbackActivity.java
+++ src/ch/ethz/twimight/activities/FeedbackActivity.java
@@ -2,14 +2,51 @@ package ch.ethz.twimight.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import ch.ethz.twimight.R;
 
 public class FeedbackActivity extends Activity {
-
+	
+	private EditText description;		
+	private Button cancelButton;
+	private Button sendButton;
+	
+	public static final String COL_TWITTER_ID = "twitter_id";
+	public static final String COL_TEXT = "description";
+	public static final String COL_TYPE = "type";
+	
+	public static final int  TYPE_BUG = 1;
+	public static final int  TYPE_FEAT_REQUEST = 2;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.feedback);
 		
+		sendButton = (Button) findViewById(R.id.feedback_send);
+		cancelButton = (Button) findViewById(R.id.feedback_cancel);
+		
+		sendButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				
+			}
+			
+		});
+		cancelButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+			
+		});
 	}
 	
 	
