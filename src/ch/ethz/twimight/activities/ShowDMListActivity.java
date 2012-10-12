a62390daefce18ace699049eb6eef9cadfd333de
ing.pcarta@gmail.com <ing.pcarta@gmail.com@124ae47a-95dc-19e3-5197-fa75647cb349>
2012-10-12 10:48:45 +0000

    removed unnecessary code for bug submission

---
+++ src/ch/ethz/twimight/activities/ShowDMListActivity.java
@@ -0,0 +1,173 @@
+/*******************************************************************************
+ * Copyright (c) 2011 ETH Zurich.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Paolo Carta - Implementation
+ *     Theus Hossmann - Implementation
+ *     Dominik Schatzmann - Message specification
+ ******************************************************************************/
+package ch.ethz.twimight.activities;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import ch.ethz.twimight.R;
+import ch.ethz.twimight.net.twitter.DMAdapter;
+import ch.ethz.twimight.net.twitter.DirectMessages;
+
+/**
+ * Shows the overview of direct messages. A list view with an item for each user with which
+ * we have exchanged direct messages.
+ * @author thossmann
+ *
+ */
+public class ShowDMListActivity extends TwimightBaseActivity{
+
+	private static final String TAG = "ShowDMListActivity";
+	
+	// Views
+	private ListView dmUserListView;
+	private ImageButton messageButton;
+
+	private DMAdapter adapter;
+	private Cursor c;
+	
+	private int rowId;
+	private String screenname;
+	public static boolean running= false;
+
+	// handler
+	static Handler handler;
+
+
+	private int positionIndex;
+	private int positionTop;
+
+	/** 
+	 * Called when the activity is first created. 
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		running = true;		
+		setContentView(R.layout.show_dm_user);
+		
+		rowId = getIntent().getIntExtra("rowId", 0);
+		screenname = getIntent().getStringExtra("screenname");
+		
+		// If we don't know which user to show, we stop the activity
+		if(rowId == 0 || screenname == null) finish();
+
+		setTitle("Direct Messages - with: " + screenname);
+		
+		dmUserListView = (ListView) findViewById(R.id.dmUserList);
+		c = getContentResolver().query(Uri.parse("content://" + DirectMessages.DM_AUTHORITY + "/" + DirectMessages.DMS + "/" + DirectMessages.DMS_USER + "/" + rowId), null, null, null, null);
+				
+		adapter = new DMAdapter(this, c);		
+		dmUserListView.setAdapter(adapter);
+		dmUserListView.setEmptyView(findViewById(R.id.dmListEmpty));
+		// Click listener when the user clicks on a user
+		
+		messageButton = (ImageButton) findViewById(R.id.headerBarMessageButtonDMUser);
+		messageButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getBaseContext(), NewDMActivity.class);
+				i.putExtra("recipient", screenname);
+				startActivity(i);
+			}
+		});
+		
+		
+	}
+	
+	/**
+	 * On resume
+	 */
+	@Override
+	public void onResume(){
+		super.onResume();
+		running = true;
+		// Are we in disaster mode?
+		LinearLayout headerBar = (LinearLayout) findViewById(R.id.headerBarDMUser);
+		if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("prefDisasterMode", false) == true) {
+			headerBar.setBackgroundResource(R.drawable.top_bar_background_disaster);
+		} else {
+			headerBar.setBackgroundResource(R.drawable.top_bar_background);
+		}
+		
+		if(positionIndex != 0 | positionTop !=0){
+			dmUserListView.setSelectionFromTop(positionIndex, positionTop);
+		}
+	}
+	
+	/**
+	 * On pause
+	 */
+	@Override
+	public void onPause(){
+		super.onPause();
+				
+	}
+
+	/**
+	 * Called at the end of the Activity lifecycle
+	 */
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+		running=false;
+		messageButton.setOnClickListener(null);
+
+		dmUserListView.setAdapter(null);
+
+		if(c!=null) c.close();
+				
+		unbindDrawables(findViewById(R.id.showDMUserListRoot));
+
+	}
+
+	
+	/**
+	 * Saves the current selection
+	 */
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+
+	  positionIndex = dmUserListView.getFirstVisiblePosition();
+	  View v = dmUserListView.getChildAt(0);
+	  positionTop = (v == null) ? 0 : v.getTop();
+	  savedInstanceState.putInt("positionIndex", positionIndex);
+	  savedInstanceState.putInt("positionTop", positionTop);
+	  
+	  Log.i(TAG, "saving" + positionIndex + " " + positionTop);
+	  
+	  super.onSaveInstanceState(savedInstanceState);
+	}
+	
+	/**
+	 * Loads the current user selection
+	 */
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState) {
+	  super.onRestoreInstanceState(savedInstanceState);
+	  
+	  positionIndex = savedInstanceState.getInt("positionIndex");
+	  positionTop = savedInstanceState.getInt("positionTop");
+	  
+	  Log.i(TAG, "restoring " + positionIndex + " " + positionTop);
+	}
+}
---
+++ src/ch/ethz/twimight/activities/ShowTweetListActivity.java
@@ -0,0 +1,607 @@
+/*******************************************************************************
+ * Copyright (c) 2011 ETH Zurich.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Paolo Carta - Implementation
+ *     Theus Hossmann - Implementation
+ *     Dominik Schatzmann - Message specification
+ ******************************************************************************/
+package ch.ethz.twimight.activities;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import ch.ethz.twimight.R;
+import ch.ethz.twimight.data.StatisticsDBHelper;
+import ch.ethz.twimight.location.LocationHelper;
+import ch.ethz.twimight.net.Html.HtmlService;
+import ch.ethz.twimight.net.twitter.TweetAdapter;
+import ch.ethz.twimight.net.twitter.TweetListView;
+import ch.ethz.twimight.net.twitter.Tweets;
+import ch.ethz.twimight.net.twitter.TwitterService;
+import ch.ethz.twimight.net.twitter.TwitterUsers;
+import ch.ethz.twimight.util.AppRater;
+import ch.ethz.twimight.util.Constants;
+
+/**
+ * The main Twimight view showing the Timeline, favorites and mentions
+ * @author thossmann
+ * 
+ */
+public class ShowTweetListActivity extends TwimightBaseActivity{
+
+	private static final String TAG = "ShowTweetListActivity";	
+	
+	// Views
+	private TweetListView timelineListView;
+	private ImageButton timelineButton;
+	private ImageButton favoritesButton;
+	private ImageButton mentionsButton;
+	private ImageButton tweetButton;
+	private ImageButton searchButton;
+
+	private TweetAdapter adapter;
+	private Cursor c;
+	
+	
+	
+	public static boolean running= false;
+	
+	// handler
+	static Handler handler;
+
+	// the menu
+	private static final int OPTIONS_MENU_PROFILE = 10;
+	private static final int OPTIONS_MENU_MESSAGES = 20;
+	private static final int OPTIONS_MENU_SETTINGS = 40;
+	private static final int OPTIONS_MENU_ABOUT = 60;
+	private static final int OPTIONS_MENU_LOGOUT = 70;
+	private static final int OPTIONS_MENU_PAIR= 80;
+	private static final int OPTIONS_MENU_HTML= 90;
+	private static final int OPTIONS_MENU_FEEDBACK= 100;
+
+	public static final int SHOW_TIMELINE = 1;
+	public static final int SHOW_FAVORITES = 2;
+	public static final int SHOW_MENTIONS = 3;
+	public static final int SHOW_USERTWEETS = 4;
+	
+	private int currentFilter = SHOW_TIMELINE;
+	private int positionIndex;
+	private int positionTop;
+	
+	//LOGS
+	LocationHelper locHelper ;
+	long timestamp;
+	Intent intent;
+	ConnectivityManager cm;
+	StatisticsDBHelper locDBHelper;	
+	CheckLocation checkLocation;
+	public static final String ON_PAUSE_TIMESTAMP = "onPauseTimestamp";
+	
+	//EVENTS
+	public static final String APP_STARTED = "app_started";
+	public static final String APP_CLOSED = "app_closed";
+	public static final String LINK_CLICKED = "link_clicked";
+	public static final String TWEET_WRITTEN = "tweet_written";
+
+	/** 
+	 * Called when the activity is first created. 
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main);	
+		
+		//statistics
+		locDBHelper = new StatisticsDBHelper(this);
+		locDBHelper.open();
+		cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+		timestamp = System.currentTimeMillis();
+		locHelper = new LocationHelper(this);
+		handler = new Handler();
+		checkLocation = new CheckLocation();
+		handler.postDelayed(checkLocation, 1*60*1000L);
+
+	    
+		setTitle("Twimight - @" + LoginActivity.getTwitterScreenname(this));
+		
+		running = true;
+		timelineListView = (TweetListView) findViewById(R.id.tweetList);
+		timelineListView.setEmptyView(findViewById(R.id.tweetListEmpty));
+		
+		// Header buttons
+		timelineButton = (ImageButton) findViewById(R.id.headerBarTimelineButton);
+		timelineButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				setFilter(SHOW_TIMELINE);
+			}
+		});
+		favoritesButton = (ImageButton) findViewById(R.id.headerBarFavoritesButton);
+		favoritesButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				setFilter(SHOW_FAVORITES);
+			}
+		});
+		mentionsButton = (ImageButton) findViewById(R.id.headerBarMentionsButton);
+		mentionsButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				setFilter(SHOW_MENTIONS);
+			}
+		});
+
+		tweetButton = (ImageButton) findViewById(R.id.headerBarTweetButton);
+		tweetButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getBaseContext(), NewTweetActivity.class));
+			}
+		});
+
+		searchButton = (ImageButton) findViewById(R.id.headerBarSearchButton);
+		searchButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				onSearchRequested();
+			}
+		});
+		
+		
+
+	}
+	
+	private class CheckLocation implements Runnable {
+
+		@Override
+		public void run() {
+			if (locHelper != null && locHelper.count > 0 && locDBHelper != null) {	
+				Log.i(TAG,"writing log");
+				locDBHelper.insertRow(locHelper.loc, cm.getActiveNetworkInfo().getTypeName(), APP_STARTED, null, timestamp);
+				locHelper.unRegisterLocationListener();
+			} else {}
+			
+		}
+		
+	}
+	
+
+	@Override
+	protected void onNewIntent(Intent intent) {		
+		setIntent(intent);
+		
+	}
+	
+
+
+	/**
+	 * On resume
+	 */
+	@Override
+	public void onResume(){
+		super.onResume();
+		running = true;
+		
+		intent = getIntent();
+		
+		// if we just got logged in, we load the timeline
+		if(intent.hasExtra("filter_request")) {
+			
+			setFilter(intent.getIntExtra("filter_request", SHOW_TIMELINE));				
+			intent.removeExtra("filter_request");
+
+		} else if(intent.hasExtra("login")){
+			
+			intent.removeExtra("login");
+			//AppRater.app_launched(this);
+			setFilter(SHOW_TIMELINE);
+		
+
+		} else {
+			setFilter(currentFilter);	
+		}
+		
+		Long pauseTimestamp =  getOnPauseTimestamp(this);
+		if (pauseTimestamp != 0 &&  (System.currentTimeMillis()-pauseTimestamp) > 10 * 60 * 1000L ) {
+			handler = new Handler();			
+			handler.post(new CheckLocation());
+			
+		}
+		
+		// Are we in disaster mode?
+		LinearLayout headerBar = (LinearLayout) findViewById(R.id.headerBar);
+		
+		if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("prefDisasterMode", false) == true) {
+			headerBar.setBackgroundResource(R.drawable.top_bar_background_disaster);
+		} else {
+			headerBar.setBackgroundResource(R.drawable.top_bar_background);
+		}
+		
+		
+		if(positionIndex != 0 | positionTop !=0){
+			timelineListView.setSelectionFromTop(positionIndex, positionTop);
+		}
+	}
+	
+	
+    
+
+
+	@Override
+	protected void onPause() {
+		
+		super.onPause();
+		setOnPauseTimestamp(System.currentTimeMillis(), this);
+	}
+
+
+	/**
+	 * 
+	 * @param id
+	 * @param context
+	 */
+	private static void setOnPauseTimestamp(long timestamp, Context context) {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		SharedPreferences.Editor prefEditor = prefs.edit();
+		prefEditor.putLong(ON_PAUSE_TIMESTAMP, timestamp);
+		prefEditor.commit();
+	}
+	
+	/**
+	 * Gets the Twitter ID from shared preferences
+	 * @param context
+	 * @return
+	 */
+	public static Long getOnPauseTimestamp(Context context) {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		return prefs.getLong(ON_PAUSE_TIMESTAMP, 0);
+	}
+
+
+	@Override
+	protected void onStop() {
+		running=false;
+		
+		super.onStop();
+	
+		
+	}
+	
+
+	/**
+	 * Called at the end of the Activity lifecycle
+	 */
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+		running = false;
+		
+		timelineButton.setOnClickListener(null);
+		favoritesButton.setOnClickListener(null);
+		mentionsButton.setOnClickListener(null);
+		tweetButton.setOnClickListener(null);
+		searchButton.setOnClickListener(null);
+
+		timelineListView.setOnItemClickListener(null);
+		timelineListView.setAdapter(null);	
+		
+	
+		if ((System.currentTimeMillis() - timestamp <= 1 * 60 * 1000L)&& locHelper!=null && locDBHelper != null && cm != null) {
+			if (locHelper.count > 0) {			
+				locHelper.unRegisterLocationListener();
+				handler.removeCallbacks(checkLocation);				
+				locDBHelper.insertRow(locHelper.loc, cm.getActiveNetworkInfo().getTypeName(),APP_STARTED , null, timestamp);
+			} else {}
+		}
+		
+		if ((locHelper != null && locHelper.count > 0) && locDBHelper != null && cm != null) {			
+			locHelper.unRegisterLocationListener();			
+			locDBHelper.insertRow(locHelper.loc, cm.getActiveNetworkInfo().getTypeName(), APP_CLOSED , null, System.currentTimeMillis());
+		} else {}
+
+		if(c!=null) c.close();
+				
+		unbindDrawables(findViewById(R.id.showTweetListRoot));
+		
+		if(PreferenceManager.getDefaultSharedPreferences(this).getBoolean("prefDisasterMode", Constants.DISASTER_DEFAULT_ON) == true)
+			Toast.makeText(this, "Warning: The disaster mode is still running in the background ", Toast.LENGTH_LONG).show();
+
+
+	}
+
+	
+	/**
+	 * Populate the Options menu
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		super.onCreateOptionsMenu(menu);
+		menu.add(1, OPTIONS_MENU_PROFILE, 1, "My Profile").setIcon(R.drawable.ic_menu_friendslist);
+		menu.add(2, OPTIONS_MENU_MESSAGES, 2, "Messages").setIcon(R.drawable.ic_menu_start_conversation);
+		menu.add(3, OPTIONS_MENU_SETTINGS, 4, "Settings").setIcon(R.drawable.ic_menu_preferences);				
+		menu.add(1, OPTIONS_MENU_PAIR, 3, "Add peer").setIcon(R.drawable.ic_menu_mark);				 
+		//menu.add(4, OPTIONS_MENU_HTML, 5, "Get WebPages").setIcon(R.drawable.ic_menu_archive);
+		menu.add(5, OPTIONS_MENU_LOGOUT, 8, "Logout").setIcon(R.drawable.ic_menu_close_clear_cancel);
+		menu.add(6, OPTIONS_MENU_ABOUT, 7, "About").setIcon(R.drawable.ic_menu_info_details);
+		menu.add(7, OPTIONS_MENU_FEEDBACK, 6, "Feedback").setIcon(R.drawable.ic_menu_edit);
+		return true;
+	}
+
+	/**
+	 * Handle options menu selection
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+
+		Intent i;
+		switch(item.getItemId()){
+		
+			
+		case OPTIONS_MENU_PROFILE:
+			Uri uri = Uri.parse("content://"+TwitterUsers.TWITTERUSERS_AUTHORITY+"/"+TwitterUsers.TWITTERUSERS);
+			Cursor c = getContentResolver().query(uri, null, TwitterUsers.COL_ID+"="+LoginActivity.getTwitterId(this), null, null);
+			if(c.getCount()!=1) return false;
+			c.moveToFirst();
+			int rowId = c.getInt(c.getColumnIndex("_id"));
+			
+			if(rowId>0){
+				// show the local user
+				i = new Intent(this, ShowUserActivity.class);
+				i.putExtra("rowId", rowId);
+				startActivity(i);
+			}
+			c.close();
+			break;
+		
+		case OPTIONS_MENU_MESSAGES:
+			// Launch User Messages
+			i = new Intent(this, ShowDMUsersListActivity.class);
+			startActivity(i);    
+			break;
+		case OPTIONS_MENU_HTML:
+			
+			i = new Intent(this, HtmlService.class);			
+			startService(i); 
+			
+			break;
+		
+		case OPTIONS_MENU_SETTINGS:
+			// Launch PrefsActivity
+			i = new Intent(this, PrefsActivity.class);
+			startActivity(i);    
+			break;
+
+		case OPTIONS_MENU_LOGOUT:
+			// In disaster mode we don't allow logging out
+			if(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean("prefDisasterMode", Constants.DISASTER_DEFAULT_ON)==false){
+				showLogoutDialog();
+			} else {
+				Toast.makeText(this, "Disable Disaster Mode first. NOTE: Once you log out Twimight will not work until you are connected to the internet again!", Toast.LENGTH_LONG).show();
+			}
+			break;
+		case OPTIONS_MENU_ABOUT:
+			// Launch AboutActivity
+			i = new Intent(this, AboutActivity.class);
+			startActivity(i);    
+			break;
+		case OPTIONS_MENU_PAIR:
+			   // Get the local Bluetooth adapter
+	        BluetoothAdapter mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+			if (mBtAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {		
+				Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+				discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);			
+				startActivityForResult(discoverableIntent, PrefsActivity.REQUEST_DISCOVERABLE);           
+
+			} else  {
+				Intent intent = new Intent(this, DeviceListActivity.class);
+				startActivity(intent);
+			}
+				
+				
+			break;
+		case OPTIONS_MENU_FEEDBACK:
+			// Launch FeedbacktActivity
+			i = new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.TDS_BASE_URL + "/bugs/new"));
+			startActivity(i);
+			//i = new Intent(this, FeedbackActivity.class);
+			//startActivity(i);    
+			break;
+		default:
+			return false;
+		}
+		return true;
+	}
+	
+
+
+	@Override
+		protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+			switch(requestCode) {
+			case PrefsActivity.REQUEST_DISCOVERABLE:
+				
+				if (resultCode != Activity.RESULT_CANCELED) {
+					Intent intent = new Intent(this, DeviceListActivity.class);
+					startActivity(intent);
+				}
+						
+			}
+		}  
+	
+	/**
+	 * Saves the current selection
+	 */
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+
+	  savedInstanceState.putInt("currentFilter", currentFilter);
+	  positionIndex = timelineListView.getFirstVisiblePosition();
+	  View v = timelineListView.getChildAt(0);
+	  positionTop = (v == null) ? 0 : v.getTop();
+	  savedInstanceState.putInt("positionIndex", positionIndex);
+	  savedInstanceState.putInt("positionTop", positionTop);
+	  
+	  Log.i(TAG, "saving" + positionIndex + " " + positionTop);
+	  
+	  super.onSaveInstanceState(savedInstanceState);
+	}
+	
+	/**
+	 * Loads the current user selection
+	 */
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState) {
+	  super.onRestoreInstanceState(savedInstanceState);
+	  
+	  currentFilter = savedInstanceState.getInt("currentFilter");
+	  positionIndex = savedInstanceState.getInt("positionIndex");
+	  positionTop = savedInstanceState.getInt("positionTop");
+	  
+	  Log.i(TAG, "restoring " + positionIndex + " " + positionTop);
+	}
+	
+	
+	
+	/**
+	 * Which tweets do we show? Timeline, favorites, mentions?
+	 * @param filter
+	 */
+	private void setFilter(int filter){
+		// set all header button colors to transparent
+		resetButtons();
+		ImageButton b=null;
+		Intent overscrollIntent = null;
+		
+		if(c!=null) c.close();
+		
+		switch(filter) {
+		case SHOW_TIMELINE: 
+			
+			b = timelineButton;
+			overscrollIntent = new Intent(this, TwitterService.class); 
+			overscrollIntent.putExtra("synch_request", TwitterService.SYNCH_TIMELINE);
+			overscrollIntent.putExtra(TwitterService.FORCE_FLAG, true);
+			c = getContentResolver().query(Uri.parse("content://" + Tweets.TWEET_AUTHORITY + "/" + Tweets.TWEETS + "/" 
+											+ Tweets.TWEETS_TABLE_TIMELINE + "/" + Tweets.TWEETS_SOURCE_ALL), null, null, null, null);
+			currentFilter=SHOW_TIMELINE;
+
+			break;
+		case SHOW_FAVORITES: 
+			
+			b = favoritesButton;
+			overscrollIntent = new Intent(this, TwitterService.class); 
+			overscrollIntent.putExtra("synch_request", TwitterService.SYNCH_FAVORITES);
+			overscrollIntent.putExtra(TwitterService.FORCE_FLAG, true);
+			c = getContentResolver().query(Uri.parse("content://" + Tweets.TWEET_AUTHORITY + "/" + Tweets.TWEETS + "/"
+											+ Tweets.TWEETS_TABLE_FAVORITES + "/" + Tweets.TWEETS_SOURCE_ALL), null, null, null, null);
+			currentFilter=SHOW_FAVORITES;
+
+			break;
+		case SHOW_MENTIONS: 
+			
+			b = mentionsButton;
+			overscrollIntent = new Intent(this, TwitterService.class); 
+			overscrollIntent.putExtra("synch_request", TwitterService.SYNCH_MENTIONS);
+			overscrollIntent.putExtra(TwitterService.FORCE_FLAG, true);
+			c = getContentResolver().query(Uri.parse("content://" + Tweets.TWEET_AUTHORITY + "/" + Tweets.TWEETS + "/" 
+											+ Tweets.TWEETS_TABLE_MENTIONS + "/" + Tweets.TWEETS_SOURCE_ALL), null, null, null, null);
+			currentFilter=SHOW_MENTIONS;
+
+			break;
+		default:
+			
+			b= timelineButton;
+			overscrollIntent = new Intent(this, TwitterService.class); 
+			overscrollIntent.putExtra("synch_request", TwitterService.SYNCH_TIMELINE);
+			overscrollIntent.putExtra(TwitterService.FORCE_FLAG, true);
+			c = getContentResolver().query(Uri.parse("content://" + Tweets.TWEET_AUTHORITY + "/" + Tweets.TWEETS + "/"
+											+ Tweets.TWEETS_TABLE_TIMELINE + "/" + Tweets.TWEETS_SOURCE_ALL), null, null, null, null);
+			currentFilter=SHOW_TIMELINE;
+
+		}
+		
+		// style button
+		if(b!=null){
+			b.setEnabled(false);
+		}
+		
+		startManagingCursor(c); 
+		adapter = new TweetAdapter(this, c);				
+		timelineListView.setAdapter(adapter);		
+		timelineListView.setOverscrollIntent(overscrollIntent);
+
+
+		// Click listener when the user clicks on a tweet
+		timelineListView.setClickable(true);
+		timelineListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+				Cursor c = (Cursor) timelineListView.getItemAtPosition(position);
+				Intent i = new Intent(getBaseContext(), ShowTweetActivity.class);
+				i.putExtra("rowId", c.getInt(c.getColumnIndex("_id")));
+				startActivity(i);
+			}
+		});
+	}
+	
+	/**
+	 * Enables all header buttons
+	 */
+	private void resetButtons(){
+		timelineButton.setEnabled(true);
+		favoritesButton.setEnabled(true);
+		mentionsButton.setEnabled(true);
+		searchButton.setEnabled(true);
+		tweetButton.setEnabled(true);
+	}
+	
+	/**
+	 * Asks the user if she really want to log out
+	 */
+	private void showLogoutDialog(){
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("Are you sure you want to log out?")
+		       .setCancelable(false)
+		       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		        	   LoginActivity.logout(getBaseContext());
+		        	   finish();
+		           }
+		       })
+		       .setNegativeButton("No", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		                dialog.cancel();
+		           }
+		       });
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+	
+}
