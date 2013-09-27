2dfb7e27fadbb29386311d58ed2ffd7702ad34b1
ing.pcarta@gmail.com <ing.pcarta@gmail.com@124ae47a-95dc-19e3-5197-fa75647cb349>
2013-09-27 15:10:57 +0000

    basic unidirectional binding with the res controller

--- src/ch/ethz/twimight/net/OMF/OmfService.java
+++ src/ch/ethz/twimight/net/OMF/OmfService.java
@@ -1,55 +1,96 @@
 package ch.ethz.twimight.net.OMF;
 
 import android.app.Service;
-import android.content.ComponentName;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
-import android.os.RemoteException;
-import android.view.View;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+import ch.ethz.twimight.activities.PrefsActivity;
+import ch.ethz.twimight.activities.ShowTweetListActivity;
+import ch.ethz.twimight.net.opportunistic.ScanningAlarm;
 
 public class OmfService extends Service {
 	
-    static final int MSG_SAY_HELLO = 1;	
-	
-	Messenger mService = null;
-
-    /** Flag indicating whether we have called bind on the service. */
-    boolean mBound;
-    
-    @Override
-	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    static final int MSG_START_APP = 1;	
+    static final int MSG_START_DIS_MODE = 2;	
+    static final int MSG_STOP_DIS_MODE = 3;	
     
     /**
-     * Class for interacting with the main interface of the service.
+     * Handler of incoming messages from external clients.
      */
-    private ServiceConnection mConnection = new ServiceConnection() {
-        public void onServiceConnected(ComponentName className, IBinder service) {          
-            mService = new Messenger(service);
-            mBound = true;
-        }
-
-        public void onServiceDisconnected(ComponentName className) {            
-            mService = null;
-            mBound = false;
-        }
-    };
-
-    public void sayHello(View v) {
-        if (!mBound) return;        
-        Message msg = Message.obtain(null, MSG_SAY_HELLO, 0, 0);
-        try {
-            mService.send(msg);
-        } catch (RemoteException e) {
-            e.printStackTrace();
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_START_APP:
+                    createActivity(getApplicationContext());
+                    break;
+                case MSG_START_DIS_MODE:
+                    enableDisMode();
+                    break;
+                case MSG_STOP_DIS_MODE:
+                    disableDisMode();
+                    break;
+              
+            }
         }
     }
+    
+	private void createActivity(Context context) {
+		Intent refreshIntent = new Intent(context,ShowTweetListActivity.class);
+		refreshIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);		
+		context.startActivity(refreshIntent);
+		
+	}
 
+    public void disableDisMode() {
+    	PrefsActivity.disableDisasterMode(getApplicationContext());
+		setPreferences(getApplicationContext(),false);
+		Toast.makeText(getApplicationContext(), "Disaster Mode disabled by OMF", Toast.LENGTH_SHORT).show();
+		
+	}
+    
+	private void setPreferences(Context context, boolean value) {
+		SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("prefDisasterMode", value);
+		editor.commit();
+	}
+
+	public void enableDisMode() {
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter.isEnabled())
+			ScanningAlarm.setBluetoothInitialState(getApplicationContext(), true);
+		else
+			ScanningAlarm.setBluetoothInitialState(getApplicationContext(), false);
+		
+		mBluetoothAdapter.enable();
+		setPreferences(getApplicationContext(),true);
+		new ScanningAlarm(getApplicationContext(),true);
+		Toast.makeText(getApplicationContext(), "Disaster Mode enabled by OMF", Toast.LENGTH_SHORT).show();
+		
+		
+	}
+
+	/**
+     * Target we publish for clients to send messages to IncomingHandler.
+     */
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    /**
+     * When binding to the service, we return an interface to our messenger
+     * for sending messages to the service.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
+        return mMessenger.getBinder();
+    }
 	
 
 }
