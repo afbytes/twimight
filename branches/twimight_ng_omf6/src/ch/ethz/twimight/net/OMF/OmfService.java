96fcdbbe60c9243cb5ab84d45ffca3009f54f41f
ing.pcarta@gmail.com <ing.pcarta@gmail.com@124ae47a-95dc-19e3-5197-fa75647cb349>
2013-09-29 15:46:32 +0000

    bidirectional binding with the res controller

--- branches/twimight_ng_omf6/src/ch/ethz/twimight/net/OMF/OmfService.java
+++ branches/twimight_ng_omf6/src/ch/ethz/twimight/net/OMF/OmfService.java
@@ -9,7 +9,9 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.Toast;
 import ch.ethz.twimight.activities.PrefsActivity;
 import ch.ethz.twimight.activities.ShowTweetListActivity;
@@ -17,31 +19,54 @@ import ch.ethz.twimight.net.opportunistic.ScanningAlarm;
 
 public class OmfService extends Service {
 	
-    static final int MSG_START_APP = 1;	
-    static final int MSG_START_DIS_MODE = 2;	
-    static final int MSG_STOP_DIS_MODE = 3;	
-    
-    /**
-     * Handler of incoming messages from external clients.
+	public static final String TAG = "OMFService";
+
+
+	static final int MSG_START_APP = 1;	
+	static final int MSG_START_DIS_MODE = 2;	
+	static final int MSG_STOP_DIS_MODE = 3;	
+	static final int MSG_REGISTER_CLIENT = 4;   
+	static final int MSG_UNREGISTER_CLIENT = 5;
+	
+	public static final int MSG_TEST_BIDIRECTIONAL = 21;
+	
+	private Messenger clientMessenger;
+	/**
+     * Target we publish for clients to send messages to IncomingHandler.
      */
-    class IncomingHandler extends Handler {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case MSG_START_APP:
-                    createActivity(getApplicationContext());
-                    break;
-                case MSG_START_DIS_MODE:
-                    enableDisMode();
-                    break;
-                case MSG_STOP_DIS_MODE:
-                    disableDisMode();
-                    break;
-              
-            }
-        }
-    }
-    
+    final Messenger localMessenger = new Messenger(new IncomingHandler());
+
+	/**
+	 * Handler of incoming messages from external clients.
+	 */
+	class IncomingHandler extends Handler {
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			
+			case MSG_START_APP:
+				createActivity(getApplicationContext());
+				break;
+			case MSG_START_DIS_MODE:
+				enableDisMode();
+				sendMessageToTarget(MSG_TEST_BIDIRECTIONAL);
+				break;
+			case MSG_STOP_DIS_MODE:
+				disableDisMode();
+				break;
+			case MSG_REGISTER_CLIENT:
+				clientMessenger = msg.replyTo;
+				break;
+			case MSG_UNREGISTER_CLIENT:
+				clientMessenger = null;
+				break;
+
+			}
+		}
+
+		
+	}
+
 	private void createActivity(Context context) {
 		Intent refreshIntent = new Intent(context,ShowTweetListActivity.class);
 		refreshIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);		
@@ -72,16 +97,20 @@ public class OmfService extends Service {
 		mBluetoothAdapter.enable();
 		setPreferences(getApplicationContext(),true);
 		new ScanningAlarm(getApplicationContext(),true);
-		Toast.makeText(getApplicationContext(), "Disaster Mode enabled by OMF", Toast.LENGTH_SHORT).show();
-		
+		Toast.makeText(getApplicationContext(), "Disaster Mode enabled by OMF", Toast.LENGTH_SHORT).show();	
 		
 	}
-
-	/**
-     * Target we publish for clients to send messages to IncomingHandler.
-     */
-    final Messenger mMessenger = new Messenger(new IncomingHandler());
-
+	
+	   public void sendMessageToTarget(int message) {
+	    	if (clientMessenger != null) return;        
+	    	Message msg = Message.obtain(null, message, 0, 0);
+	    	try {
+	    		clientMessenger.send(msg);
+	    		Log.i(TAG,"Message sent to the RC");
+	    	} catch (RemoteException e) {    		
+	    	}
+	    }
+	
     /**
      * When binding to the service, we return an interface to our messenger
      * for sending messages to the service.
@@ -89,7 +118,7 @@ public class OmfService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
-        return mMessenger.getBinder();
+        return localMessenger.getBinder();
     }
 	
 
