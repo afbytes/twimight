5ef183f2f49fcd285e2b7fb609ef7bfe0a30ca8b
steven.meliopoulos <steven.meliopoulos@124ae47a-95dc-19e3-5197-fa75647cb349>
2013-10-11 13:26:04 +0000

    fixed missing content when displaying tweet details from user tweet list

--- trunk/src/ch/ethz/twimight/activities/TweetDetailActivity.java
+++ trunk/src/ch/ethz/twimight/activities/TweetDetailActivity.java
@@ -8,42 +8,50 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import ch.ethz.twimight.R;
 import ch.ethz.twimight.fragments.ShowTweetFragment.OnTweetDeletedListener;
 import ch.ethz.twimight.fragments.TweetListFragment;
 import ch.ethz.twimight.fragments.adapters.ShowTweetPageAdapter;
 import ch.ethz.twimight.net.twitter.Tweets;
 
-public class TweetDetailActivity extends TwimightBaseActivity implements OnTweetDeletedListener {
-	
-	
+public class TweetDetailActivity extends TwimightBaseActivity implements
+		OnTweetDeletedListener {
+
 	ContentResolver resolver;
-	//String query;
-	
+	long userId;
+
+	// String query;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);		
-		
+		setContentView(R.layout.main);
+
 		ViewPager viewPager;
 		resolver = getContentResolver();
 		Intent intent = getIntent();
-		
-		long rowId = intent.getIntExtra("rowId", 0);		
+
+		long rowId = intent.getIntExtra("rowId", 0);
 		int type = intent.getIntExtra("type", TweetListFragment.TIMELINE_KEY);
-		//if (type == TweetListFragment.SEARCH_TWEETS)
-			//query = intent.getStringExtra(ListFragment.SEARCH_QUERY);
+		// if (type == TweetListFragment.SEARCH_TWEETS)
+		// query = intent.getStringExtra(ListFragment.SEARCH_QUERY);
+		if (type == TweetListFragment.USER_TWEETS) {
+			userId = intent.getLongExtra(TweetListFragment.USER_ID, -1);
+		}
 		
-		ArrayList<Long> rowIdList = getRowIds(type);		
-		if (rowIdList != null) {			
-			ShowTweetPageAdapter pageAdapter = new ShowTweetPageAdapter(getFragmentManager(), rowIdList );
-			viewPager = (ViewPager) findViewById(R.id.viewpager);			
+
+		ArrayList<Long> rowIdList = getRowIds(type);
+		if (rowIdList != null) {
+			ShowTweetPageAdapter pageAdapter = new ShowTweetPageAdapter(
+					getFragmentManager(), rowIdList);
+			viewPager = (ViewPager) findViewById(R.id.viewpager);
 			viewPager.setAdapter(pageAdapter);
-			//viewPager.setOffscreenPageLimit(2);			
+			// viewPager.setOffscreenPageLimit(2);
 			viewPager.setCurrentItem(rowIdList.indexOf(rowId));
 		}
-			
+
 	}
 
 	@Override
@@ -53,27 +61,26 @@ public class TweetDetailActivity extends TwimightBaseActivity implements OnTweet
 		String title = getString(R.string.tweet);
 		getActionBar().setTitle(title);
 	}
-	
+
 	private ArrayList<Long> getRowIds(int type) {
-		Cursor c ;
-		ArrayList<Long> list=null;
-		
-		c= performQuery(type);
-		
-		if (c != null && c.getCount() >0){
-			
+		Cursor c;
+		ArrayList<Long> list = null;
+
+		c = performQuery(type);
+
+		if (c != null && c.getCount() > 0) {
+
 			c.moveToFirst();
 			list = cursorToList(c);
 		}
 		return list;
-		
-		
+
 	}
 
 	private ArrayList<Long> cursorToList(Cursor c) {
 		ArrayList<Long> list = new ArrayList<Long>();
-		
-		while(c.isAfterLast() != true) {
+
+		while (c.isAfterLast() != true) {
 			long id = c.getLong(c.getColumnIndex("_id"));
 			list.add(id);
 			c.moveToNext();
@@ -82,37 +89,52 @@ public class TweetDetailActivity extends TwimightBaseActivity implements OnTweet
 	}
 
 	private Cursor performQuery(int type) {
-		Cursor c= null;
+		Cursor c = null;
 
-		switch(type) {
+		switch (type) {
 		case TweetListFragment.TIMELINE_KEY:
 
-			c = resolver.query(Uri.parse("content://" + Tweets.TWEET_AUTHORITY + "/" + Tweets.TWEETS + "/" 
-					+ Tweets.TWEETS_TABLE_TIMELINE + "/" + Tweets.TWEETS_SOURCE_ALL), null, null, null, null);
+			c = resolver
+					.query(Uri.parse("content://" + Tweets.TWEET_AUTHORITY
+							+ "/" + Tweets.TWEETS + "/"
+							+ Tweets.TWEETS_TABLE_TIMELINE + "/"
+							+ Tweets.TWEETS_SOURCE_ALL), null, null, null, null);
 
 			break;
-		case TweetListFragment.FAVORITES_KEY:			
-			c = resolver.query(Uri.parse("content://" + Tweets.TWEET_AUTHORITY + "/" + Tweets.TWEETS + "/"
-					+ Tweets.TWEETS_TABLE_FAVORITES + "/" + Tweets.TWEETS_SOURCE_ALL), null, null, null, null);
+		case TweetListFragment.FAVORITES_KEY:
+			c = resolver
+					.query(Uri.parse("content://" + Tweets.TWEET_AUTHORITY
+							+ "/" + Tweets.TWEETS + "/"
+							+ Tweets.TWEETS_TABLE_FAVORITES + "/"
+							+ Tweets.TWEETS_SOURCE_ALL), null, null, null, null);
 			break;
-		case TweetListFragment.MENTIONS_KEY: 			
-			c = resolver.query(Uri.parse("content://" + Tweets.TWEET_AUTHORITY + "/" + Tweets.TWEETS + "/" 
-					+ Tweets.TWEETS_TABLE_MENTIONS + "/" + Tweets.TWEETS_SOURCE_ALL), null, null, null, null);
+		case TweetListFragment.MENTIONS_KEY:
+			c = resolver
+					.query(Uri.parse("content://" + Tweets.TWEET_AUTHORITY
+							+ "/" + Tweets.TWEETS + "/"
+							+ Tweets.TWEETS_TABLE_MENTIONS + "/"
+							+ Tweets.TWEETS_SOURCE_ALL), null, null, null, null);
 			break;
 		case TweetListFragment.SEARCH_TWEETS:
-			c = resolver.query(Uri.parse("content://" + Tweets.TWEET_AUTHORITY + "/" + Tweets.TWEETS + "/" 
-					+ Tweets.SEARCH), null, SearchableActivity.query , null, null);
+			c = resolver.query(
+					Uri.parse("content://" + Tweets.TWEET_AUTHORITY + "/"
+							+ Tweets.TWEETS + "/" + Tweets.SEARCH), null,
+					SearchableActivity.query, null, null);
 			break;
-
+		case TweetListFragment.USER_TWEETS:
+			c = resolver.query(
+					Uri.parse("content://" + Tweets.TWEET_AUTHORITY + "/"
+							+ Tweets.TWEETS + "/" + Tweets.TWEETS_TABLE_USER
+							+ "/" + userId), null, null, null, null);
 		}
 		return c;
-		
+
 	}
 
 	@Override
 	public void onDelete() {
 		finish();
-		
+
 	}
 
 }
